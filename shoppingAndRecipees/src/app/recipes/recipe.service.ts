70f74e6fcc7b7ffef2f1a870eabc4b387fc283b4
Elvedin <serious_nerd@hotmail.com>
2018-12-06 21:47:48 +0100

    added services to course project

---
+++ shoppingAndRecipees/src/app/recipes/recipe.service.ts
@@ -0,0 +1,36 @@
+import {Recipe} from './recipe.model';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+               'najnoviji recept baš je dobar',
+               'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+               [
+                 new Ingredient('Meat', 1),
+                 new Ingredient('French Fries', 20)
+               ]),
+    new Recipe('Another  Test Recipe',
+               'drugi recept',
+               'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+               [
+                 new Ingredient('Buns', 2),
+                 new Ingredient('Meat', 1)
+               ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
