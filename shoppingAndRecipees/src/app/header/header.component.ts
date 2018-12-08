f258e86cc065f9630409ebb01918330ad773fb66
Elvedin <serious_nerd@hotmail.com>
2018-12-08 22:48:43 +0100

    routing added to course project

--- shoppingAndRecipees/src/app/header/header.component.ts
+++ shoppingAndRecipees/src/app/header/header.component.ts
@@ -1,14 +1,9 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {template} from '@angular/core/src/render3';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 }
